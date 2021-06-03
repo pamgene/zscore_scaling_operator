6b98da2370d395045c648d45127ce47a5f892b5b
ginberg <info@gerinberg.com>
2021-06-03 09:57:56 +0000

    add calculation by color

--- main.R
+++ main.R
@@ -17,10 +17,24 @@ dimension <- ifelse(is.null(ctx$op.value('Dimension')), 'Row', as.character(ctx$
 data <- ctx %>% 
   select(.ci, .ri, .y)
 
+color <- FALSE
+if (length(ctx$colors) == 1) {
+  data  <- data %>% mutate(group = ctx$select(ctx$colors) %>% pull())
+  color <- TRUE
+}
+
 if (dimension == "Row") {
-  data <- data %>% group_by(.ri)
+  if (color) {
+    data <- data %>% group_by(.ri, group) 
+  } else {
+    data <- data %>% group_by(.ri) 
+  }
 } else if (dimension == "Column") {
-  data <- data %>% group_by(.ci)
+  if (color) {
+    data <- data %>% group_by(.ci, group) 
+  } else {
+    data <- data %>% group_by(.ci) 
+  }
 } else {
   stop("Invalid option for dimension")
 }
@@ -28,5 +42,7 @@ if (dimension == "Row") {
 new_col_name <- paste(dimension, "Scale", sep = ".")
 data %>% 
   do(do.scale(., new_col_name)) %>%
+  ungroup(.) %>% 
+  select(.ri, .ci, all_of(new_col_name)) %>%
   ctx$addNamespace() %>%
   ctx$save()
--- workspace.R
+++ workspace.R
@@ -23,11 +23,25 @@ dimension <- ifelse(is.null(ctx$op.value('Dimension')), 'Row', as.character(ctx$
 
 data <- ctx %>% 
   select(.ci, .ri, .y)
-  
+
+color <- FALSE
+if (length(ctx$colors) == 1) {
+  data  <- data %>% mutate(group = ctx$select(ctx$colors) %>% pull())
+  color <- TRUE
+}
+
 if (dimension == "Row") {
-  data <- data %>% group_by(.ri)
+  if (color) {
+    data <- data %>% group_by(.ri, group) 
+  } else {
+    data <- data %>% group_by(.ri) 
+  }
 } else if (dimension == "Column") {
-  data <- data %>% group_by(.ci)
+  if (color) {
+    data <- data %>% group_by(.ci, group) 
+  } else {
+    data <- data %>% group_by(.ci) 
+  }
 } else {
   stop("Invalid option for dimension")
 }
@@ -35,5 +49,7 @@ if (dimension == "Row") {
 new_col_name <- paste(dimension, "Scale", sep = ".")
 data %>% 
   do(do.scale(., new_col_name)) %>%
+  ungroup(.) %>% 
+  select(.ri, .ci, all_of(new_col_name)) %>%
   ctx$addNamespace() %>%
   ctx$save()
